1045dd8d8b427f27b18aae6864223810edea271f
hx <781742009@qq.com>
2018-10-10 11:26:13 +0800

    cal

--- cal/ViewController.swift
+++ cal/ViewController.swift
@@ -9,8 +9,131 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+   
+    
+   
+    @IBOutlet weak var result: UITextField!
+    var re=0//判断player.etxt前是否存在符号
+    
+    @IBAction func qc(_ sender: Any) {
+        
+    }
+    
+    @IBAction func jj(_ sender: Any) {
+        
+    }
+    
+    @IBAction func percent(_ sender: Any) {
+    }
+    
+    @IBAction func cf(_ sender: Any) {
+    }
+    
+    @IBAction func multiply(_ sender: Any) {
+    }
+    
+    @IBAction func nine(_ sender: Any) {
+        if re==1{
+            result.text="9"
+        }else{
+            result.text = result.text!+"9"
+        }
+    }
+    
+    
+    @IBAction func eight(_ sender: Any) {
+        if re==1{
+            result.text="8"
+        }else{
+            result.text = result.text!+"8"
+        }
+    }
+    
+    
+    @IBAction func seven(_ sender: Any) {
+        if re==1{
+            result.text="7"
+        }else{
+            result.text = result.text!+"7"
+        }
+    }
+    
+    
+    @IBAction func four(_ sender: Any) {
+        if re==1{
+            result.text="4"
+        }else{
+            result.text = result.text!+"4"
+        }
+    }
+    
+    
+    @IBAction func five(_ sender: Any) {
+        if re==1{
+            result.text="5"
+        }else{
+            result.text = result.text!+"5"
+        }
+    }
+    
+    
+    @IBAction func six(_ sender: Any) {
+        if re==1{
+            result.text="6"
+        }else{
+            result.text = result.text!+"6"
+        }
+    }
+    
+    
+    @IBAction func one(_ sender: Any) {
+        if re==1{
+            result.text="1"
+        }else{
+        result.text = result.text!+"1"
+        }
+    }
+    
+    @IBAction func two(_ sender: Any) {
+        if re==1{
+            result.text="2"
+        }else{
+            result.text = result.text!+"2"
+        }
+    }
+    
+    
+    @IBAction func three(_ sender: Any) {
+        if re==1{
+            result.text="3"
+        }else{
+            result.text = result.text!+"3"
+        }
+    }
+    
+    
+    @IBAction func zero(_ sender: Any) {
+        if re==1{
+            result.text="0"
+        }else{
+            result.text = result.text!+"0"
+        }
+    }
+    
+    
+    @IBAction func point(_ sender: Any) {
+    }
+    
+    @IBAction func dy(_ sender: Any) {
+    }
+    
+    @IBAction func jia(_ sender: Any) {
+    }
+    
+    @IBAction func jian(_ sender: Any) {
+    }
     override func viewDidLoad() {
+       
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
